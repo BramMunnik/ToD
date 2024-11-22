a4816297d693846b1085e8ce8d9058927ba6cc7e
Bram Munnik <2204854munnik@zuyd.nl>
2024-11-22 18:52:13 +0100

    Sterren interactable

--- ToD/QuestionsPage.xaml.cs
+++ ToD/QuestionsPage.xaml.cs
@@ -6,5 +6,36 @@ namespace ToD
         {
             InitializeComponent();
         }
+        private void StarClicked(object sender, EventArgs e)
+        {
+            // Controleer op welk sterretje is geklikt
+            if (sender is ImageButton clickedStar)
+            {
+                // Haal de sterindex op (bijv. Star1, Star2, ...)
+                string starName = clickedStar.StyleId; // Of gebruik .x:Name als je die hier verwerkt
+                int starIndex = int.Parse(starName.Replace("Star", ""));
+
+                // Verander de sterren
+                UpdateStars(starIndex);
+            }
+        }
+
+        private void UpdateStars(int selectedStarIndex)
+        {
+            // Loop door alle sterren en pas ze aan
+            for (int i = 1; i <= 5; i++)
+            {
+                var star = (ImageButton)this.FindByName($"Star{i}");
+                if (i <= selectedStarIndex)
+                {
+                    star.Source = "star_filled.png"; // Gevulde ster
+                }
+                else
+                {
+                    star.Source = "star_empty.png"; // Lege ster
+                }
+            }
+        }
     }
+
 }
