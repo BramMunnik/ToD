c91de0c72441fb5420e49a1757014b7a55e5c95d
JouwNaam <JouwEmail@example.com>
2025-01-10 11:40:56 +0100

    Models update

--- ToD/Model/Models.cs
+++ ToD/Model/Models.cs
@@ -1,91 +1,88 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ToD.Model
 {
     public class User
     {
-        [Key]
+        [PrimaryKey]
         public Guid UserID { get; set; }
 
-        [Required]
+        [NotNull]
         public string Name { get; set; }
 
-        // Navigation property: one user can host many sessions
+        // Navigation property (manueel te verwerken)
+        [Ignore]
         public ICollection<Session> Sessions { get; set; }
     }
 
     public class Session
     {
-        [Key]
+        [PrimaryKey]
         public Guid SessionID { get; set; }
 
-        [Required]
-        [ForeignKey(nameof(User))]
+        [NotNull]
         public Guid HostID { get; set; }
 
+        [Ignore]
         public User Host { get; set; }
 
-        [Required]
-        public string SelectedCategories { get; set; } // JSON string of categories
+        [NotNull]
+        public string SelectedCategories { get; set; } // JSON string
 
-        [Range(1, 5)]
+        [NotNull]
         public int DaringLevel { get; set; }
 
-        [Required]
-        public string QuestionPool { get; set; } // JSON string of QuestionIDs
+        [NotNull]
+        public string QuestionPool { get; set; } // JSON string
 
         public string QRCode { get; set; }
 
-        // Navigation property
+        [Ignore]
         public ICollection<Participant> Participants { get; set; }
     }
 
     public class Participant
     {
-        [Key]
+        [PrimaryKey]
         public Guid ParticipantID { get; set; }
 
-        [Required]
-        [ForeignKey(nameof(Session))]
+        [NotNull]
         public Guid SessionID { get; set; }
 
+        [Ignore]
         public Session Session { get; set; }
 
-        [ForeignKey(nameof(User))]
-        public Guid? UserID { get; set; } // Nullable for anonymous users
+        public Guid? UserID { get; set; } // Nullable voor anonieme gebruikers
 
+        [Ignore]
         public User User { get; set; }
 
-        [Required]
+        [NotNull]
         public Guid TemporaryID { get; set; }
     }
 
     public class GameData
     {
-        [Key]
+        [PrimaryKey]
         public Guid QuestionID { get; set; }
 
-        [Required]
+        [NotNull]
         public string QuestionText { get; set; }
 
-        [Required]
+        [NotNull]
         public string Category { get; set; }
 
-        [Range(1, 5)]
+        [NotNull]
         public int DaringLevel { get; set; }
 
-        [ForeignKey(nameof(User))]
         public Guid? CreatedBy { get; set; }
 
+        [Ignore]
         public User Creator { get; set; }
 
-        [Required]
+        [NotNull]
         public string QuestionType { get; set; }
 
         public string PhotoLink { get; set; }
@@ -93,23 +90,23 @@ namespace ToD.Model
 
     public class TemporaryStorage
     {
-        [Key]
-        [ForeignKey(nameof(Session))]
+        [PrimaryKey]
         public Guid SessionID { get; set; }
 
+        [Ignore]
         public Session Session { get; set; }
 
-        [ForeignKey(nameof(GameData))]
         public Guid? ActiveQuestionID { get; set; }
 
+        [Ignore]
         public GameData ActiveQuestion { get; set; }
 
-        [ForeignKey(nameof(User))]
         public Guid? CurrentPlayerID { get; set; }
 
+        [Ignore]
         public User CurrentPlayer { get; set; }
 
-        [Required]
-        public string TurnOrder { get; set; } // JSON array of UserIDs or TemporaryIDs
+        [NotNull]
+        public string TurnOrder { get; set; } // JSON array
     }
 }
